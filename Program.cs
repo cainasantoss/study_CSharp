20793ac83be9061e464954386eb022cb43bd083f
cainã santos <scaina82@gmail.com>
2024-06-06 20:10:58 -0300

    variaveis

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 17;
+            string name = "caina";
+            bool lindo = true;
+
+            Console.WriteLine($"{name} ele tem {idade}, e bonito? {lindo}");
+        }
+    }
+}
